ac188687ee10c006eebc3a149d02680082a4e05c
Adrian Zambrano <jadrianzc99@gmail.com>
2021-08-03 10:59:44 -0500

    Cambios en login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,7 +13,10 @@ const Login = () => {
 				</div>
 				<div className="container-login">
 					<form>
-						<h1>Iniciar Sesión</h1>
+						<div>
+							<h1>Iniciar Sesión</h1>
+							<hr className="separador" />
+						</div>
 						<div className="container-login-items">
 							<div className="container-login-email">
 								<FontAwesomeIcon icon={faEnvelope} className="iconCar" />
@@ -24,7 +27,7 @@ const Login = () => {
 								<input type="password" name="" value="" placeholder="contraseña" />
 							</div>
 
-							<div>
+							<div className="container-login-submit">
 								<input type="submit" name="" value="Login" />
 							</div>
 						</div>
