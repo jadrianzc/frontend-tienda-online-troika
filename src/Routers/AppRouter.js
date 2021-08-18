9a3d63f19210bed1533cc6f06d99dda287470007
untalyorly <yorliparraga@gmail.com>
2021-08-17 19:51:44 -0500

    Mejora de rutas y administrar usuarios

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,58 +1,88 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import NavBar from '../components/NavBar/NavBar';
-import Inicio from '../components/Inicio/Inicio';
-import Agencias from '../components/Agencias/Agencias';
-import QuienesSomos from '../components/QuienesSomos/QuienesSomos';
-import Login from '../components/Login/Login';
-import Registro from '../components/Registro/Registro';
-import Producto from '../components/Categorias/Producto/Producto';
-import Categorias from '../components/Categorias/Catregorias';
-import Busqueda from '../components/Busqueda/Busqueda';
-import PerfilUsuario from '../components/PerfilUsuario/PerfilUsuario';
-import Administrador from '../components/Administrador/Administrador';
-import Usuario from '../components/Administrador/Usuario/Usuario';
-import InformacionPago from '../components/Pago/Informacion/InformacionPago';
-import Pago from '../components/Pago/Pago/Pago';
-import CarritoCompras from '../components/CarritoCompras/CarritoCompras';
+import NavBar from "../components/NavBar/NavBar";
+import Inicio from "../components/Inicio/Inicio";
+import Agencias from "../components/Agencias/Agencias";
+import QuienesSomos from "../components/QuienesSomos/QuienesSomos";
+import Login from "../components/Login/Login";
+import Registro from "../components/Registro/Registro";
+import Producto from "../components/Categorias/Producto/Producto";
+import Categorias from "../components/Categorias/Catregorias";
+import Busqueda from "../components/Busqueda/Busqueda";
+import PerfilUsuario from "../components/PerfilUsuario/PerfilUsuario";
+import Administrador from "../components/Administrador/Administrador";
+import Usuario from "../components/Administrador/Usuario/Usuario";
+import InformacionPago from "../components/Pago/Informacion/InformacionPago";
+import Pago from "../components/Pago/Pago/Pago";
+import CarritoCompras from "../components/CarritoCompras/CarritoCompras";
 
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
-import RouteCli from './RouteCli';
-import { useState } from 'react';
+import PrivateRoute from "./PrivateRoute";
+import PrivateRouteRender from "./PrivateRouteRender";
+import PublicRoute from "./PublicRoute";
+import RouteCli from "./RouteCli";
+import RouteCliCompo from "./RouteCliCompo";
+import { useState } from "react";
 
 export default function AppRouter() {
-	const [cantCar, setCantCar] = useState(0);
-	const [addCar, setAddCar] = useState(false);
+  const [cantCar, setCantCar] = useState(0);
+  const [addCar, setAddCar] = useState(false);
 
-	return (
-		<Router>
-			<Switch>
-				<PrivateRoute hasRole="admin" path="/Admin" exact component={Administrador} />
-				<PrivateRoute hasRole="admin" path="/Admin/Usuario" exact component={Usuario} />
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute
+          hasRole="admin"
+          path="/Admin"
+          exact
+          component={Administrador}
+        />
+        <PrivateRoute
+          hasRole="admin"
+          path="/Admin/Usuario"
+          exact
+          component={Usuario}
+        />
 
-				<>
-					<NavBar cantCar={cantCar} setCantCar={setCantCar} addCar={addCar} />
-					<RouteCli path="/" exact render={() => <Inicio setCantCar={setCantCar} />} />
-					<RouteCli path="/Quienes-somos" exact component={QuienesSomos} />
-					<RouteCli path="/Agencias" exact component={Agencias} />
-					<PublicRoute path="/Login" exact component={Login} />
-					<PublicRoute path="/Registro" exact component={Registro} />
-					<RouteCli path="/Producto/:id" exact render={() => <Producto addCar={addCar} setAddCar={setAddCar} />} />
-					<RouteCli path="/Categorías/:id" exact component={Categorias} />
-					<RouteCli path="/Busqueda/:nombre" exact component={Busqueda} />
-					<PrivateRoute path="/PerfilUsuario" exact component={PerfilUsuario} />
+        <>
+          <NavBar cantCar={cantCar} setCantCar={setCantCar} addCar={addCar} />
+          <RouteCli
+            path="/"
+            exact
+            render={() => <Inicio setCantCar={setCantCar} />}
+          />
+          <RouteCliCompo path="/Quienes-somos" exact component={QuienesSomos} />
+          <RouteCliCompo path="/Agencias" exact component={Agencias} />
+          <PublicRoute path="/Login" exact component={Login} />
+          <PublicRoute path="/Registro" exact component={Registro} />
+          <RouteCli
+            path="/Producto/:id"
+            exact
+            render={() => <Producto addCar={addCar} setAddCar={setAddCar} />}
+          />
+          <RouteCliCompo path="/Categorías/:id" exact component={Categorias} />
+          <RouteCliCompo path="/Busqueda/:nombre" exact component={Busqueda} />
+          <PrivateRoute path="/PerfilUsuario" exact component={PerfilUsuario} />
 
-					<PrivateRoute
-						path="/carrito-compras"
-						exact
-						render={() => <CarritoCompras addCar={addCar} setAddCar={setAddCar} />}
-					/>
-					<PrivateRoute path="/carrito-compras/info-pago" exact component={InformacionPago} />
-					<PrivateRoute path="/carrito-compras/info-pago/pago" exact component={Pago} />
-				</>
-			</Switch>
-			{/* <Footer />  */}
-		</Router>
-	);
+          <PrivateRouteRender
+            path="/carrito-compras"
+            exact
+            render={() => (
+              <CarritoCompras addCar={addCar} setAddCar={setAddCar} />
+            )}
+          />
+          <PrivateRoute
+            path="/carrito-compras/info-pago"
+            exact
+            component={InformacionPago}
+          />
+          <PrivateRoute
+            path="/carrito-compras/info-pago/pago"
+            exact
+            component={Pago}
+          />
+        </>
+      </Switch>
+      {/* <Footer />  */}
+    </Router>
+  );
 }
--- src/components/Administrador/Usuario/Usuario.js
+++ src/components/Administrador/Usuario/Usuario.js
@@ -1,69 +1,175 @@
-import React from 'react';
-import { Container, Grid, Typography } from '@material-ui/core';
-import './Usuario.css';
+import React, { useEffect, useState } from "react";
+import { Container, Grid, Typography } from "@material-ui/core";
+import TablaUsuarios from "./TablaUsuarios";
+import "./Usuario.css";
+import axios from "axios";
+import Alert from "@material-ui/lab/Alert";
+import Snackbar from "@material-ui/core/Snackbar";
 
 const Usuario = () => {
-	return (
-		<Container className="container container-user">
-			<Grid container justifyContent="space-between" className="grid-container-add-user">
-				<Grid item className="grid-item-user-4">
-					<div className="grid-item-title">
-						<Typography className="title">Agregar usuario</Typography>
-						<hr className="underline" />
-					</div>
-					<Grid item className="grid-item-user-data">
-						<Grid container className="grid-container-user-data">
-							<Grid item className="grid-item-info">
-								<label>Nombres</label>
-								<input type="text"></input>
-							</Grid>
-							<Grid item className="grid-item-info">
-								<label>Apellidos</label>
-								<input type="text"></input>
-							</Grid>
-							<Grid item className="grid-item-info">
-								<label>Cédula/RUC</label>
-								<input type="text"></input>
-							</Grid>
-							<Grid item className="grid-item-info">
-								<label>E-mail*</label>
-								<input type="text"></input>
-							</Grid>
-							<Grid item className="grid-item-info">
-								<label>Contraseña </label>
-								<input type="password"></input>
-							</Grid>
-						</Grid>
-					</Grid>
-					<Grid item className="grid-item-user-btn">
-						<Grid container className="grid-container-user-btn" justifyContent="flex-end" alignItems="center">
-							<button>Agregar</button>
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item className="grid-item-user-6">
-					<div className="grid-item-title">
-						<Typography className="title">Búsqueda</Typography>
-						<hr className="underline" />
-					</div>
-					<Grid item className="grid-item-user-buscar">
-						<Grid container className="grid-container-user-buscar" justifyContent="space-between" alignItems="center">
-							<Grid item className="grid-item-user-label">
-								<label>ID:</label>
-							</Grid>
-							<Grid item className="grid-item-user-input">
-								<input type="text"></input>
-							</Grid>
-							<Grid item className="grid-container-user-btn" justifyContent="flex-end" alignItems="center">
-								<button>Buscar</button>
-							</Grid>
-						</Grid>
-					</Grid>
-					<Grid item className="grid-item-user-tabla"></Grid>
-				</Grid>
-			</Grid>
-		</Container>
-	);
+  const [openAlert, setOpenAlert] = useState(false);
+  const [datos, setDatos] = useState({
+    nomb_usuario: "",
+    apell_usuario: "",
+    ced_usuario: "",
+    email_usuario: "",
+    contraseña_usuario: "",
+    rol_usuario: "admin",
+  });
+  const [stado, setStado] = useState(false);
+  const handleInputChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(datos);
+    try {
+      await axios
+        .get(
+          "http://localhost:4000/api/v1/verificaUsuario/" + datos.email_usuario
+        )
+        .then((respp) => {
+          if (respp.data.length === 1) {
+            console.log("correo existe");
+          } else {
+            RegistrarUsuario();
+            e.target.name = "";
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const RegistrarUsuario = async () => {
+    try {
+      await axios.post("http://localhost:4000/api/v1/usuarios", datos);
+      console.log("enviaso");
+      setDatos({
+        nomb_usuario: "",
+        apell_usuario: "",
+        ced_usuario: "",
+        email_usuario: "",
+        contraseña_usuario: "",
+        rol_usuario: "admin",
+      });
+      setStado(!stado);
+      setOpenAlert(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleCloseAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenAlert(false);
+  };
+  return (
+    <Container className="container container-user">
+      <Grid
+        container
+        justifyContent="space-between"
+        className="grid-container-add-user"
+      >
+        <Grid item className="grid-item-user-4">
+          <div className="grid-item-title">
+            <Typography className="title-usuario title">
+              Agregar usuario
+            </Typography>
+          </div>
+          <Grid item className="grid-item-user-data">
+            <form method="post" onSubmit={handleSubmit}>
+              <Grid container className="grid-container-user-data">
+                <Grid item className="grid-item-info">
+                  <label>Nombres</label>
+                  <input
+                    type="text"
+                    name="nomb_usuario"
+                    value={datos.nomb_usuario}
+                    required
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item className="grid-item-info">
+                  <label>Apellidos</label>
+                  <input
+                    type="text"
+                    name="apell_usuario"
+                    value={datos.apell_usuario}
+                    required
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item className="grid-item-info">
+                  <label>Cédula/RUC</label>
+                  <input
+                    type="text"
+                    name="ced_usuario"
+                    value={datos.ced_usuario}
+                    required
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item className="grid-item-info">
+                  <label>E-mail*</label>
+                  <input
+                    type="text"
+                    name="email_usuario"
+                    value={datos.email_usuario}
+                    required
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item className="grid-item-info">
+                  <label>Contraseña </label>
+                  <input
+                    type="password"
+                    name="contraseña_usuario"
+                    value={datos.contraseña_usuario}
+                    required
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                className="grid-container-user-btn"
+                justifyContent="flex-end"
+                alignItems="center"
+              >
+                <button type="submit">Agregar</button>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
+        <Grid item className="grid-item-user-6">
+          <div className="grid-item-title">
+            <Typography className="title-usuario title">
+              Búsqueda de usuario
+            </Typography>
+          </div>
+          <Grid item className="grid-item-user-buscar">
+            <Grid item className="grid-item-user-tabla">
+              <TablaUsuarios stado={stado} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+      >
+        <Alert variant="filled" onClose={handleCloseAlert} severity="success">
+          El usuario se creó correctamente.
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
 };
 
 export default Usuario;
