6abbe07c7df4d12fa2f72b3f635ba0354e492d99
Adrian Zambrano <jadrianzc99@gmail.com>
2021-08-24 21:10:14 -0500

    change

--- src/components/HistorialTransacciones/HistorialTransacciones.js
+++ src/components/HistorialTransacciones/HistorialTransacciones.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Grid, Typography, Modal, Snackbar, Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
+import '../../components/Administrador/Usuario/Usuario.css';
+import axios from 'axios';
+import Alert from '@material-ui/lab/Alert';
+import TablaHistorial from './TablaHistorial';
 
-const HistorialTransacciones = () => {
+function HistorialTransacciones() {
+	const [openModal, setOpenModal] = useState(false);
+	const [openAlert, setOpenAlert] = useState(false);
+	const [stado, setStado] = useState(false);
+
+	const [valorBusqueda, setValorBusqueda] = useState({});
+	const [valueRadio, setValueRadio] = useState('pendiente');
+
+	const handleRadioChange = (e) => {
+		console.log(e.target.value);
+		setValueRadio(e.target.value);
+	};
+
 	return (
-		<div>
-			<div>HISTORIAL USUARIO</div>
-		</div>
+		<Container className="container container-user">
+			<Grid container>
+				<Grid item container className="grid-item-produc-4" xs={12}>
+					<Grid item className="grip-busca-pedido">
+						<div className="grid-item-title">
+							<Typography className="title-usuario title">Búsqueda de pedidos</Typography>
+						</div>
+						<Grid container>
+							<Grid item alignItems="center" xs={8}>
+								<form
+									//onSubmit={handleSubmitBusqueda}
+									style={{ display: 'flex', flexDirection: 'row' }}
+								>
+									<label>Código:</label>
+									<input type="text" name="buscodigo"></input>
+
+									<Grid item className="grid-container-user-btn">
+										<button type="submit">Buscar</button>
+										<button
+											type="reset"
+											defaultValue="Reset"
+											// onClick={() => {
+											//   setStado(!stado);
+											//   //setBuscar("");
+											//   setValorBusqueda("");
+											// }}
+										>
+											Limpiar
+										</button>
+									</Grid>
+								</form>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid item className="grid-item-produc-6" xs={12}>
+					<Grid item className="grid-item-user-tabla">
+						<TablaHistorial stado={stado} valorBusqueda={valorBusqueda} valueRadio={valueRadio} />
+					</Grid>
+				</Grid>
+			</Grid>
+
+			{/* Modal Agregar*/}
+			{/* <Modal
+        open={openModal}
+        //onClose={handleClose}
+        className="ModalPadreRegi"
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {bodyAgregar}
+      </Modal>
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+      >
+        <Alert variant="filled" onClose="" severity="success">
+          El producto se agregó correctamente.
+        </Alert>
+      </Snackbar> */}
+		</Container>
 	);
-};
+}
 
 export default HistorialTransacciones;
---
+++ src/components/HistorialTransacciones/TablaHistorial.js
@@ -0,0 +1,237 @@
+import React, { useEffect, useState } from 'react';
+import { Grid, Modal, Snackbar, TextareaAutosize } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import { Table, TableContainer, TableHead, TableCell, TableBody, TableRow } from '@material-ui/core';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+function TablaHistorial(props) {
+	const [openModal, setOpenModal] = useState(false);
+	const [openAlert, setOpenAlert] = useState(false);
+
+	const [estado, setEtado] = useState(false);
+
+	const [documentos, setDocumentos] = useState([]);
+	const [tablaPedidos, setTablaPedidos] = useState([]);
+	const [datos, setDatos] = useState({ carrito_usuario: [] });
+
+	const cookies = new Cookies();
+	const idUserSession = cookies.get('id');
+
+	useEffect(() => {
+		const LoadData = async () => {
+			console.log(props.valueRadio);
+			try {
+				const res = await axios.get(`http://localhost:4000/api/v1/pedidos/user/${idUserSession}`);
+				setDocumentos(res.data);
+				setTablaPedidos(res.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		LoadData();
+	}, [props.stado, props.valueRadio, estado]);
+
+	/*
+	 *Pagado
+	 */
+	// const Pagado = async (data) => {
+	// 	let pago = { estado: data.estado === 'pendiente' ? 'pagado' : 'entregado' };
+	// 	try {
+	// 		await axios.put(`http://localhost:4000/api/v1/pedidos/${data._id}`, pago);
+	// 		setEtado(!estado);
+	// 		setOpenModal(!openModal);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
+	/** */
+
+	/*
+	 *Detalle
+	 */
+
+	const bodyDetalle = (
+		<div className="ModalAgregaProduct">
+			<h1 className="title-usuario title-Produc">Actualizar producto</h1>
+			<Grid container className="grid-container-user-data user-edit">
+				<Grid container item xs={12} spacing={3}>
+					<Grid container item xs={6} spacing={3}>
+						<Grid item className="grid-item-info" xs={12}>
+							<label>Cedula:</label>
+							<input
+								type="text"
+								//name="codigo_producto"
+								value={datos.ced_usuario}
+								disabled
+							/>
+						</Grid>
+						<Grid item className="grid-item-info" xs={12}>
+							<label>Nombre Cliente:</label>
+							<input
+								type="text"
+								//name="marca_auto"
+								value={datos.nomb_usuario}
+								disabled
+							/>
+						</Grid>
+					</Grid>
+					<Grid container item xs={6} spacing={3}>
+						<Grid item className="grid-item-info" xs={12} direction="row">
+							<label>Productos:</label>
+							<TextareaAutosize
+								maxRows={7}
+								minRows={7}
+								disabled
+								style={{ resize: 'none' }}
+								defaultValue={datos.carrito_usuario
+									.map(
+										(prod) => '(' + prod.cantidad_producto.toString() + ') ' + prod.descrip_producto.toString() + '\n\n'
+									)
+									.join('')}
+							/>
+						</Grid>
+					</Grid>
+					<Grid container item xs={6} spacing={3}>
+						<Grid item className="grid-item-info" xs={12}>
+							<label>Celular:</label>
+							<input
+								type="text"
+								//name="modelo_auto"
+								value={datos.cel_usuario}
+								disabled
+							/>
+						</Grid>
+						<Grid item className="grid-item-info" xs={12}>
+							<label>Direccion:</label>
+							<input
+								type="text"
+								//name="nom_producto"
+								value={datos.direcc_usuario}
+								disabled
+							/>
+						</Grid>
+					</Grid>
+					<Grid container item xs={6} spacing={3}>
+						<Grid item className="grid-item-info" xs={6}>
+							<label>Cantidad:</label>
+							<input
+								type="text"
+								//name="modelo_producto"
+								value={datos.carrito_usuario.length}
+								disabled
+							/>
+						</Grid>
+						<Grid item className="grid-item-info" xs={6}>
+							<label>Precio:</label>
+							<input
+								type="number"
+								//name="cantidad_producto"
+								value={datos.total_carrito}
+								disabled
+							/>
+						</Grid>
+						<Grid item className="grid-item-info" xs={6}>
+							<label>Fecha:</label>
+							<input
+								type="text"
+								//name="precio_producto"
+								value={datos.f_creacion_ordenCompra}
+								disabled
+							/>
+						</Grid>
+						<Grid item className="grid-item-info" xs={6}>
+							<label>Estado:</label>
+							<input
+								type="text"
+								//name="descrip_producto"
+								value={datos.estado}
+								disabled
+							/>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+
+			<input type="button" className=" btnPwd btnPwdCan" value="Cancelar" onClick={() => setOpenModal(false)} />
+		</div>
+	);
+	/** */
+
+	return (
+		<div style={{ height: 400, width: '100%' }}>
+			<TableContainer style={{ maxHeight: '100%', width: '100%' }}>
+				<Table stickyHeader>
+					<TableHead className="TableHead">
+						<TableRow>
+							<TableCell>CI. Cliente</TableCell>
+							<TableCell>Cantidad</TableCell>
+							<TableCell>Productos</TableCell>
+							<TableCell>Precio</TableCell>
+							<TableCell>Fecha</TableCell>
+							<TableCell>Estado</TableCell>
+							<TableCell>Acciones</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{documentos.map((doc) => (
+							<TableRow key={doc._id}>
+								<TableCell>{doc.ced_usuario}</TableCell>
+								<TableCell>{doc.carrito_usuario.length}</TableCell>
+								<TableCell style={{ maxHeight: '50px' }}>
+									{doc.carrito_usuario.slice(0, 3).map((prod) => (
+										<p style={{ margin: '0' }}>
+											{'(' + prod.cantidad_producto.toString() + ') ' + prod.descrip_producto.toString()}
+										</p>
+									))}
+								</TableCell>
+								<TableCell>{doc.total_carrito}</TableCell>
+								<TableCell>{doc.f_creacion_ordenCompra}</TableCell>
+								<TableCell>
+									{doc.estado === 'pagado' ? (
+										<span style={{ color: 'darkorange', fontWeight: 'bold' }}>Pagado</span>
+									) : doc.estado === 'entregado' ? (
+										<span style={{ color: 'green', fontWeight: 'bold' }}>Entregado</span>
+									) : (
+										<span style={{ color: 'red', fontWeight: 'bold' }}>Pendiente</span>
+									)}
+								</TableCell>
+								<TableCell>
+									<button
+										onClick={() => {
+											setOpenModal(true);
+											setDatos(doc);
+										}}
+									>
+										Ver Detalle
+									</button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			{/* Modal Detalle */}
+			<Modal
+				open={openModal}
+				//onClose={handleClose}
+				className="ModalPadreRegi"
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				{bodyDetalle}
+			</Modal>
+			{/* <Snackbar
+        open={openAlert}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+      >
+        <Alert variant="filled" onClose={handleCloseAlert} severity="success">
+          El producto se actualizó correctamente.
+        </Alert>
+      </Snackbar> */}
+		</div>
+	);
+}
+
+export default TablaHistorial;
