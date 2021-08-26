1ec14bedab00ebb7cb0ef6bf592c810281646afa
untalyorly <yorliparraga@gmail.com>
2021-08-25 19:26:53 -0500

    rutas

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -29,6 +29,7 @@ import PublicRoute from "./PublicRoute";
 import RouteCli from "./RouteCli";
 import RouteCliCompo from "./RouteCliCompo";
 import { useState } from "react";
+import Cookies from "universal-cookie";
 
 export default function AppRouter() {
   const [cantCar, setCantCar] = useState(0);
@@ -37,108 +38,119 @@ export default function AppRouter() {
   const [menuState, setMenuState] = useState(true);
   const [estadoProduCar, setEstadoProduCar] = useState(false);
 
+  const cookies = new Cookies();
+  const user = cookies.get("coki");
+  console.log(user);
+
   return (
     <Router>
+      {user && user.rol_usuario === "admin" ? (
+        <></>
+      ) : (
+        <NavBar cantCar={cantCar} setCantCar={setCantCar} addCar={addCar} />
+      )}
       <Switch>
+        {/* Normal */}
+        <RouteCli
+          exact
+          path="/"
+          render={() => <Inicio setCantCar={setCantCar} />}
+        />
+        <RouteCliCompo exact path="/Quienes-somos" component={QuienesSomos} />
+        <RouteCliCompo exact path="/Agencias" component={Agencias} />
+        <PublicRoute exact path="/Login" component={Login} />
+        <PublicRoute exact path="/Registro" component={Registro} />
+        <Route
+          exact
+          path="/Producto/:id"
+          render={() => (
+            <Producto
+              addCar={addCar}
+              setAddCar={setAddCar}
+              setEstadoProduCar={setEstadoProduCar}
+            />
+          )}
+        />
+        <RouteCliCompo path="/Categorías/:id" exact component={Categorias} />
+        <RouteCliCompo path="/Busqueda/:nombre" exact component={Busqueda} />
+        <PrivateRoute path="/PerfilUsuario" exact component={PerfilUsuario} />
         <PrivateRoute
+          exact
+          path="/HistorialTransacciones"
+          component={HistorialTransacciones}
+        />
+
+        <Route
+          exact
+          path="/carrito-compras"
+          render={() => (
+            <CarritoCompras
+              addCar={addCar}
+              setAddCar={setAddCar}
+              setEstadoProduCar={setEstadoProduCar}
+              estadoProduCar={estadoProduCar}
+            />
+          )}
+        />
+        <PrivateRoute
+          exact
+          path="/carrito-compras/info-pago"
+          render={() => <InformacionPago setDataInfo={setDataInfo} />}
+        />
+        <PrivateRoute
+          exact
+          path="/carrito-compras/info-pago/pago"
+          render={() => (
+            <Pago
+              dataInfo={dataInfo}
+              menuState={menuState}
+              setAddCar={setAddCar}
+              addCar={addCar}
+            />
+          )}
+        />
+        <PrivateRoute
+          exact
+          path="/carrito-compras/info-pago/pago/envio"
+          component={PagoFinal}
+        />
+        {/*  */}
+
+        {/* Administracion */}
+        <PrivateRoute
+          exact
           hasRole="admin"
           path="/Admin"
-          exact
           component={Administrador}
         />
         <PrivateRoute
+          exact
           hasRole="admin"
           path="/Admin/Productos"
-          exact
           component={AdminProductos}
         />
         <PrivateRoute
+          exact
           hasRole="admin"
           path="/Admin/Usuario"
-          exact
           component={Usuario}
         />
         <PrivateRoute
+          exact
           hasRole="admin"
           path="/Admin/Categorias"
-          exact
           component={AdminCategorias}
         />
         <PrivateRoute
+          exact
           hasRole="admin"
           path="/Admin/Pedidos"
-          exact
           component={AdminPedidos}
         />
+        {/*  */}
 
-        <>
-          <NavBar cantCar={cantCar} setCantCar={setCantCar} addCar={addCar} />
-          <RouteCli
-            path="/"
-            exact
-            render={() => <Inicio setCantCar={setCantCar} />}
-          />
-          <RouteCliCompo path="/Quienes-somos" exact component={QuienesSomos} />
-          <RouteCliCompo path="/Agencias" exact component={Agencias} />
-          <PublicRoute path="/Login" exact component={Login} />
-          <PublicRoute path="/Registro" exact component={Registro} />
-          <Route
-            path="/Producto/:id"
-            exact
-            render={() => (
-              <Producto
-                addCar={addCar}
-                setAddCar={setAddCar}
-                setEstadoProduCar={setEstadoProduCar}
-              />
-            )}
-          />
-          <RouteCliCompo path="/Categorías/:id" exact component={Categorias} />
-          <RouteCliCompo path="/Busqueda/:nombre" exact component={Busqueda} />
-          <PrivateRoute path="/PerfilUsuario" exact component={PerfilUsuario} />
-          <PrivateRoute
-            path="/HistorialTransacciones"
-            exact
-            component={HistorialTransacciones}
-          />
-
-          <Route
-            path="/carrito-compras"
-            exact
-            render={() => (
-              <CarritoCompras
-                addCar={addCar}
-                setAddCar={setAddCar}
-                setEstadoProduCar={setEstadoProduCar}
-                estadoProduCar={estadoProduCar}
-              />
-            )}
-          />
-          <PrivateRoute
-            path="/carrito-compras/info-pago"
-            exact
-            render={() => <InformacionPago setDataInfo={setDataInfo} />}
-          />
-          <PrivateRoute
-            path="/carrito-compras/info-pago/pago"
-            exact
-            render={() => (
-              <Pago
-                dataInfo={dataInfo}
-                menuState={menuState}
-                setAddCar={setAddCar}
-                addCar={addCar}
-              />
-            )}
-          />
-          <PrivateRoute
-            path="/carrito-compras/info-pago/pago/envio"
-            exact
-            component={PagoFinal}
-          />
-          <Route path="*" component={Error404} />
-        </>
-        <Route path="*" component={Error404} />
+        {/* Error 404 */}
+        <Route component={Error404} />
       </Switch>
     </Router>
   );
